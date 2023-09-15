3131cb25e8082f03ee6c81f7ffa83f2ccd8bed88
Halil Topcu <visualhalil@outlook.com>
2023-09-15 23:04:12 +0300

    Credit Card Number Validation React Native Example

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ccNumValidator from 'cc-validation';
+import React, { useState } from 'react';
+import {View, TextInput, Button, Text, StyleSheet} from 'react-native';
 
-export default function App() {
+const CreditCardValidationScreen = () => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [isValid, setIsValid] = useState(null);
+
+  const validateCard = () => {
+    const isValidCard = ccNumValidator(cardNumber);
+    setIsValid(isValidCard);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        inputMode={"numeric"}
+        placeholder="Kredi Kartı Numarası"
+        onChangeText={(cardNum) => setCardNumber(cardNum)}
+        value={cardNumber}
+      />
+      <Button title="Doğrula" onPress={validateCard} />
+      {(isValid !== null) && (
+        <Text style={styles.resultText}>
+          Kredi kartı numarası {isValid ? 'geçerli' : 'geçersiz'}.
+        </Text>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    input: {
+      width: 300,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      marginBottom: 10,
+    },
+    resultText: {
+      marginTop: 20,
+      fontSize: 18,
+    }
+  });
+  
+  export default CreditCardValidationScreen;
